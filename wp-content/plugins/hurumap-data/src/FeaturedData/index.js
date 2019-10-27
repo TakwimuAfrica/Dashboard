fe18c0911f375c12ef6d1fae411e8aa223240e3d
khadija <khadijamahanga@y7mail.com>
2019-10-27 17:57:06 +0300

    use innerblock for featured data

--- wp-content/plugins/hurumap-data/src/FeaturedData/index.js
+++ wp-content/plugins/hurumap-data/src/FeaturedData/index.js
@@ -1,28 +1,76 @@
+import React from 'react';
+
 import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
-import EditChart from './EditChart';
-import SaveChart from './SaveChart';
+import { InnerBlocks } from '@wordpress/editor';
 
-registerBlockType('hurumap-ui/chart', {
-  title: __('HURUmap Chart', 'hurumap-ui'),
+import { useState } from '@wordpress/element';
+
+const TEMPLATE_OPTIONS = [
+  {
+    title: 'All Flourish Chart',
+    icon: 'pie',
+    template: [
+      [
+        'core/columns',
+        {},
+        [
+          ['core/column', {}, [['hurumap-data/flourish-block', {}]]],
+          ['core/column', {}, [['hurumap-data/flourish-block', {}]]]
+        ]
+      ]
+    ]
+  },
+  {
+    title: 'All HURUmap Chart',
+    icon: 'pie',
+    template: [
+      [
+        'core/columns',
+        {},
+        [
+          ['core/column', {}, [['hurumap-data/hurumap-block', {}]]],
+          ['core/column', {}, [['hurumap-data/hurumap-block', {}]]]
+        ]
+      ]
+    ]
+  },
+  {
+    title: 'Mixed Chart',
+    icon: 'pie',
+    template: [
+      [
+        'core/columns',
+        {},
+        [
+          ['core/column', {}, [['hurumap-data/hurumap-block', {}]]],
+          ['core/column', {}, [['hurumap-data/flourish-block', {}]]]
+        ]
+      ]
+    ]
+  }
+];
+
+registerBlockType('hurumap-data/featured-data', {
+  title: __('Featured Chart', 'hurumap-data'),
   icon: 'chart-pie', // https://developer.wordpress.org/resource/dashicons/#chart-pie
   category: 'widgets',
-  supports: {
-    align: ['wide', 'full', 'left', 'right', 'center'],
-    html: false
+  edit: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [template, setTemplate] = useState(null);
+    return (
+      <InnerBlocks
+        template={template}
+        __experimentalTemplateOptions={TEMPLATE_OPTIONS}
+        __experimentalOnSelectTemplateOption={setTemplate}
+      />
+    );
   },
-  attributes: {
-    geoId: {
-      type: 'string'
-    },
-    chartId: {
-      type: 'string'
-    },
-    chartWidth: {
-      type: 'string',
-      default: '100%'
-    }
-  },
-  edit: EditChart,
-  save: SaveChart
+  save: () => {
+    return (
+      <div>
+        <InnerBlocks.Content />
+      </div>
+    );
+  }
 });
