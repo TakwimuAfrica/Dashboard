b57825a24f0d3b2b2fb3874f80e4d1a6a54ae910
khadija <khadijamahanga@y7mail.com>
2019-10-28 12:28:48 +0300

    custom select template

--- wp-content/plugins/hurumap-data/src/FeaturedData/index.js
+++ wp-content/plugins/hurumap-data/src/FeaturedData/index.js
@@ -2,26 +2,13 @@ import React from 'react';
 import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
 import { InnerBlocks } from '@wordpress/editor';
-
-const TEMPLATE = [
-  ['core/heading', { placeholder: 'Enter title...', level: 3 }],
-  [
-    'core/columns',
-    {},
-    [
-      ['core/column', {}, [['hurumap-data/hurumap-block', {}]]],
-      ['core/column', {}, [['hurumap-data/flourish-block', {}]]]
-    ]
-  ]
-];
+import Edit from './Edit';
 
 registerBlockType('hurumap-data/featured-data', {
   title: __('Featured Chart', 'hurumap-data'),
   icon: 'chart-pie', // https://developer.wordpress.org/resource/dashicons/#chart-pie
   category: 'widgets',
-  edit: () => {
-    return <InnerBlocks template={TEMPLATE} templateLock="insert" />;
-  },
+  edit: Edit,
   save: () => {
     return (
       <div>
