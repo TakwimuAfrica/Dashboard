637bd83607ce3b3141560de01cfa2671aafbc9b8
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-10-29 12:20:26 +0300

    patch fix for new table version

--- wp-content/plugins/hurumap-data/db.php
+++ wp-content/plugins/hurumap-data/db.php
@@ -19,14 +19,17 @@ function activate_hurumap_data()
         ) $charset_collate;";
 
         $sql .= "CREATE TABLE `{$wpdb->base_prefix}flourish_charts`(
-            `id`        varchar(45) NOT NULL ,
+            `id`         varchar(45) NOT NULL ,
+            `section`    varchar(45) NULL ,
             `title`      varchar(255) NOT NULL ,
-            `country`   varchar(255) NOT NULL ,
-            `media_id`       int NOT NULL ,
+            `subtitle`   varchar(255) NOT NULL ,
+            `file`       json NOT NULL ,
             `published` tinyint NOT NULL DEFAULT 0,
             `created_at` datetime NOT NULL DEFAULT NOW() ON UPDATE NOW(),
             `updated_at` datetime NOT NULL DEFAULT NOW(),
-            PRIMARY KEY (`id`)
+            PRIMARY KEY (`id`),
+            KEY `fkIdx_22` (`section`),
+            CONSTRAINT `FK_22` FOREIGN KEY `fkIdx_22` (`section`) REFERENCES `{$wpdb->base_prefix}chart_sections` (`id`)
         ) $charset_collate;";
 
         $sql .= "CREATE TABLE `{$wpdb->base_prefix}hurumap_charts`(
@@ -49,6 +52,28 @@ function activate_hurumap_data()
         $success = empty($wpdb->last_error);
 
         update_site_option('hurumap_data_db_version', 1);
+    }
+
+    if ($version < 2) {
+        $wpdb->query("DROP TABLE IF EXISTS `{$wpdb->base_prefix}flourish_charts`");
+
+        $sql = "CREATE TABLE `{$wpdb->base_prefix}flourish_charts`(
+            `id`        varchar(45) NOT NULL ,
+            `title`      varchar(255) NOT NULL ,
+            `country`   varchar(255) NOT NULL ,
+            `media_id`       int NOT NULL ,
+            `published` tinyint NOT NULL DEFAULT 0,
+            `created_at` datetime NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+            `updated_at` datetime NOT NULL DEFAULT NOW(),
+
+            PRIMARY KEY (`id`)
+            ) $charset_collate;";
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        dbDelta($sql);
+        $success = empty($wpdb->last_error);
+
+        update_site_option('hurumap_data_db_version', 2);
+
     }
 
     return $success;
