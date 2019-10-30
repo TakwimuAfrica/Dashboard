f94113815780bc8c15ee913d81cf188e962e836d
Khadija Mahanga <khadijamahanga@y7mail.com>
2019-10-30 08:17:02 +0300

    Flourish Charts and Featured Data (#12)
    * flourish api endpoints and callbacks
    
    * update flourish db structure
    
    * update flourish chart form
    
    * register blocks
    
    * yarn updates
    
    * add new flourish endpoint
    
    * register flourish + featured data
    
    * update custom build script
    
    * flourish block save & edit template
    
    * index block type
    
    * small fixes
    
    * ad country to flourish
    
    * fixes
    
    * more customisation
    
    * push flourish changes
    
    * remove hurumap-ui plugin
    
    * use innerblock for featured data
    
    * update flourish table
    
    * changes
    
    * changes
    
    * remove dump
    
    * featured data fix
    
    * custom select template
    
    * update api parameter
    
    * fixes
    
    * Update wp-content/plugins/hurumap-data/package.json
    
    Co-Authored-By: Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
    
    * rename build
    
    * delete function
    
    * fix spacing
    
    * save div
    
    * update id
    
    * remove acf field select work
    
    * patch fix for new table version
    
    * update build folder
    
    * display flex
    
    * update flex on grid
    
    * title for flourish charts
    
    * update delete function + include - of chart_id regex
    
    * fix title + rename hurumap-ui to hurumap-data
    
    * close bracket position
    
    * Refactor folder hurumap-data to hurumap + introduce migrate() in db.php
    
    * git rm -rf --cached .
    
    * git rm -rf --cached .
    
    * git rm -rf --cached .
    
    * themes/hurumap
    
    * git rm -rf --cached .
    
    * save file name to database
    
    * remove preview
    
    * reload iframe after file update
    
    * Debounce saving flourish data
    
    * showing file name

--- wp-content/plugins/hurumap-ui/src/Theme.js
+++
@@ -1,3 +0,0 @@
-import { createTheme } from '@codeforafrica/hurumap-ui/core';
-
-export default createTheme({});
---
+++ wp-content/plugins/hurumap/src/FeaturedData/Edit.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { __ } from '@wordpress/i18n';
+import { Fragment } from '@wordpress/element';
+import { PanelBody, SelectControl } from '@wordpress/components';
+import { InspectorControls, InnerBlocks } from '@wordpress/editor';
+
+import withRoot from '../withRoot';
+import propTypes from '../propTypes';
+
+const TEMPLATE_OPTIONS = {
+  hurumap: [
+    ['core/heading', { placeholder: 'Enter title...', level: 3 }],
+    [
+      'core/columns',
+      {},
+      [
+        ['core/column', {}, [['hurumap-data/hurumap-block', {}]]],
+        ['core/column', {}, [['hurumap-data/hurumap-block', {}]]]
+      ]
+    ]
+  ],
+
+  flourish: [
+    ['core/heading', { placeholder: 'Enter title...', level: 3 }],
+    [
+      'core/columns',
+      {},
+      [
+        ['core/column', {}, [['hurumap-data/flourish-block', {}]]],
+        ['core/column', {}, [['hurumap-data/flourish-block', {}]]]
+      ]
+    ]
+  ],
+
+  mixed: [
+    ['core/heading', { placeholder: 'Enter title...', level: 3 }],
+    [
+      'core/columns',
+      {},
+      [
+        ['core/column', {}, [['hurumap-data/hurumap-block', {}]]],
+        ['core/column', {}, [['hurumap-data/flourish-block', {}]]]
+      ]
+    ]
+  ]
+};
+
+function Edit({ attributes: { template }, setAttributes }) {
+  return (
+    <Fragment>
+      <InspectorControls>
+        <PanelBody title={__('Featured Data', 'hurumap-data')} />
+      </InspectorControls>
+      <SelectControl
+        label={__('Template', 'hurumap-data')}
+        value={template}
+        options={[
+          { value: null, label: 'Select a template', disable: true },
+          { value: 'hurumap', label: 'HURUmap Charts' },
+          { value: 'mixed', label: 'Mixed Charts' },
+          { value: 'flourish', label: 'Flourish Charts' }
+        ]}
+        onChange={selectedTemplate => {
+          setAttributes({ template: selectedTemplate });
+        }}
+      />
+
+      <InnerBlocks
+        template={TEMPLATE_OPTIONS[template]}
+        templateLock="insert"
+      />
+    </Fragment>
+  );
+}
+
+Edit.propTypes = {
+  attributes: propTypes.shape({
+    template: propTypes.string
+  }).isRequired,
+  setAttributes: propTypes.func.isRequired
+};
+
+export default withRoot(Edit);
