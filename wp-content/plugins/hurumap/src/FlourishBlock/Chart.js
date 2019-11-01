6513204fc16363febdb21561f4925665fc9e9267
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-11-01 11:51:31 +0300

    Code split the data page + reduce flourish chart code

--- wp-content/plugins/hurumap/src/FlourishBlock/Chart.js
+++ wp-content/plugins/hurumap/src/FlourishBlock/Chart.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-danger */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
@@ -27,91 +27,6 @@ const useStyles = makeStyles(theme => ({
 
 function Chart({ title, chartId, iframeKey }) {
   const classes = useStyles();
-  const [animated, setAnimated] = useState(false);
-  const [animatedId, setAnimatedId] = useState('');
-
-  useEffect(
-    () => {
-      let timer1;
-      if (animated) {
-        timer1 = setTimeout(() => setAnimated(true), 1000);
-      }
-
-      return () => {
-        if (timer1) {
-          clearTimeout(timer1);
-        }
-      };
-    },
-    [animated] // useEffect will run only one time
-  );
-
-  const updateIframe = (iframe, wrapper) => {
-    /* eslint-disable no-param-reassign */
-    // In rear cases we don't have `wrapper` element to reference from, just
-    // provide a default height to start
-    const height =
-      wrapper && wrapper.offsetHeight > 420 ? wrapper.offsetHeight : 420;
-    iframe.style.height = `${height}px`;
-    iframe.contentDocument.body.style.background = 'rgb(0,0,0) !important';
-    const headers = iframe.contentDocument.getElementsByClassName(
-      'flourish-header'
-    );
-    if (headers && headers.length) {
-      headers[0].style.display = 'none';
-    }
-  };
-
-  const handleIframeLoaded = e => {
-    const iframe = e.target;
-    if (iframe) {
-      // Most static charts have a wrapper element with id `wrapper`
-      const wrapper = iframe.contentDocument.getElementById('wrapper');
-      if (wrapper) {
-        updateIframe(iframe, wrapper);
-      } else {
-        // The animated charts may or may not contain a wrapping element.
-        // In case there is one, store its id in the component state for easier
-        // lookup
-        let wrapperId = '';
-        if (
-          iframe.contentDocument.getElementById('flourish-popup-constrainer')
-        ) {
-          wrapperId = 'flourish-popup-constrainer';
-        }
-        setAnimated(true);
-        setAnimatedId(wrapperId);
-      }
-
-      // Add htm2canvas
-      const frameHead = iframe.contentDocument
-        .getElementsByTagName('head')
-        .item(0);
-      const script = iframe.contentDocument.createElement('script');
-      script.type = 'text/javascript';
-      script.src =
-        'https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.1/dist/html2canvas.min.js';
-      frameHead.appendChild(script);
-
-      // Override `body` inline style
-      const style = iframe.contentDocument.createElement('style');
-      style.type = 'text/css';
-      style.append('body[style] { background: none !important; }');
-      frameHead.appendChild(style);
-    }
-  };
-  if (animated) {
-    const animatedIframe = document.getElementById(
-      `data-indicator-flourish-${chartId}`
-    );
-    if (animatedIframe) {
-      updateIframe(
-        animatedIframe,
-        animatedIframe.contentDocument.getElementById(animatedId)
-      );
-    }
-  }
-
   return (
     <div className={classes.flourishContainer}>
       <Grid container direction="column" alignItems="center">
@@ -119,12 +34,10 @@ function Chart({ title, chartId, iframeKey }) {
           {title}
         </Typography>
         <iframe
-          id={`data-indicator-flourish-${chartId}`}
           key={iframeKey}
           frameBorder="0"
           scrolling="no"
           title={title}
-          onLoad={handleIframeLoaded}
           src={`/wp-json/hurumap-data/flourish/${chartId}/`}
           className={classes.root}
         />
