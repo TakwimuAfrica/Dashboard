02f1e104b914b5d0a0e6778d64b2159f9c9adc04
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-11-01 11:21:19 +0300

    Fix config-flourish.js

--- wp-content/themes/hurumap/assets/js/config-flourish.js
+++ wp-content/themes/hurumap/assets/js/config-flourish.js
@@ -2,14 +2,15 @@ window.onload = () => {
   /**
    * Setup same origin frame height
    */
-  if (window.frameElement) { 
+  if (window.frameElement || window.parent) { 
+    const iframe = window.frameElement || window.parent.document.querySelector(`iframe[src*="${chartId}"]`)
     const wrapper =
       document.getElementById("wrapper") ||
-      iframe.contentDocument.getElementById("flourish-popup-constrainer");
+      document.getElementById("flourish-popup-constrainer");
     const height =
       wrapper && wrapper.offsetHeight > 420 ? wrapper.offsetHeight : 420;
 
-    window.frameElement.style.height = `${height}px`;
+    iframe.style.height = `${height}px`;
   }
 
   document.body.style.background = "rgb(0,0,0) !important";
