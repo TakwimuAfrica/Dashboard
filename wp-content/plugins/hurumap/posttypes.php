ea744a9124e54d4f4965e56f63f71916c3cd6971
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-11-06 13:59:24 +0300

    Create post types code to allow development evironment to have them

--- wp-content/plugins/hurumap/posttypes.php
+++ wp-content/plugins/hurumap/posttypes.php
@@ -2,6 +2,148 @@
 
 function create_posttype()
 {
+    /**
+	 * Post Type: Profile Sections.
+	 */
+
+	$labels = array(
+		"name" => __( "Profile Sections", "hurumap" ),
+		"singular_name" => __( "Profile Section", "hurumap" ),
+		"parent_item_colon" => __( "Parent Profile", "hurumap" ),
+		"parent_item_colon" => __( "Parent Profile", "hurumap" ),
+	);
+
+	$args = array(
+		"label" => __( "Profile Sections", "hurumap" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "profile_section_page", "with_front" => false ),
+		"query_var" => true,
+		"supports" => array( "title", "editor" ),
+	);
+
+	register_post_type( "profile_section_page", $args );
+
+	/**
+	 * Post Type: Topics.
+	 */
+
+	$labels = array(
+		"name" => __( "Topics", "hurumap" ),
+		"singular_name" => __( "Topic", "hurumap" ),
+	);
+
+	$args = array(
+		"label" => __( "Topics", "hurumap" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "topic_page", "with_front" => true ),
+		"query_var" => true,
+		"supports" => array( "title", "editor" ),
+	);
+
+	register_post_type( "topic_page", $args );
+
+	/**
+	 * Post Type: Carousel Topics.
+	 */
+
+	$labels = array(
+		"name" => __( "Carousel Topics", "hurumap" ),
+		"singular_name" => __( "Carousel Topic", "hurumap" ),
+	);
+
+	$args = array(
+		"label" => __( "Carousel Topics", "hurumap" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "carousel_topic", "with_front" => true ),
+		"query_var" => true,
+		"supports" => array( "title" ),
+	);
+
+	register_post_type( "carousel_topic", $args );
+
+	/**
+	 * Post Type: Profiles.
+	 */
+
+	$labels = array(
+		"name" => __( "Profiles", "hurumap" ),
+		"singular_name" => __( "Profile", "hurumap" ),
+	);
+
+	$args = array(
+		"label" => __( "Profiles", "hurumap" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "profile", "with_front" => true ),
+		"query_var" => true,
+		"supports" => array( "title" ),
+	);
+
+    register_post_type( "profile", $args );
+    
+    /**
+     * Post Type: Snippet
+     */
+
     register_post_type(
         'snippet',
         array(
