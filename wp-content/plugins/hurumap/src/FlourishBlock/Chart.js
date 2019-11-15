8c2651a085285346e98af843530916c372d83ea8
khadija <khadijamahanga@y7mail.com>
2019-11-15 16:19:37 +0300

    update block insight container to include description + source for hurumap

--- wp-content/plugins/hurumap/src/FlourishBlock/Chart.js
+++ wp-content/plugins/hurumap/src/FlourishBlock/Chart.js
@@ -3,72 +3,43 @@ import React from 'react';
 
 import InsightContainer from '@codeforafrica/hurumap-ui/core/InsightContainer';
 import makeStyles from '@material-ui/styles/makeStyles';
-import ArrowDropUp from '@material-ui/icons/ArrowDropUp';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import propTypes from '../propTypes';
 
-const useStyles = makeStyles(({ palette, breakpoints }) => ({
+const useStyles = makeStyles({
   root: {
     width: '100%',
     padding: '1.25rem'
   },
-  iframe: {},
-  descriptionWrapper: {
-    margin: '1.25rem auto 0',
-    [breakpoints.up('md')]: {
-      width: '90%'
-    }
-  },
-  description: {
-    color: palette.data.main,
-    display: 'flex',
-    marginLeft: '1.25rem'
-  }
-}));
+  iframe: {}
+});
 
 function Chart({ title, description, chartId, iframeKey, ...props }) {
   const classes = useStyles(props);
   return (
-    <>
-      <InsightContainer
-        hideInsight
-        key={chartId}
-        variant="analysis"
-        loading={false}
-        title={title}
-        embedCode="embed text"
-        actions={{
-          handleShare: () => {}
-        }}
-      >
-        <div />
-        <div className={classes.root}>
-          <iframe
-            key={iframeKey}
-            frameBorder="0"
-            scrolling="no"
-            title={title}
-            src={`/wp-json/hurumap-data/flourish/${chartId}/`}
-            className={classes.iframe}
-          />
-        </div>
-      </InsightContainer>
-      {description && (
-        <div className={classes.descriptionWrapper}>
-          <Grid container alignItems="flex-start" wrap="nowrap">
-            <Grid item>
-              <ArrowDropUp color="primary" />
-            </Grid>
-            <Grid item>
-              <Typography variant="caption" className={classes.description}>
-                {description}
-              </Typography>
-            </Grid>
-          </Grid>
-        </div>
-      )}
-    </>
+    <InsightContainer
+      hideInsight
+      key={chartId}
+      variant="analysis"
+      loading={false}
+      title={title}
+      description={description}
+      embedCode="embed text"
+      actions={{
+        handleShare: () => {}
+      }}
+    >
+      <div />
+      <div className={classes.root}>
+        <iframe
+          key={iframeKey}
+          frameBorder="0"
+          scrolling="no"
+          title={title}
+          src={`/wp-json/hurumap-data/flourish/${chartId}/`}
+          className={classes.iframe}
+        />
+      </div>
+    </InsightContainer>
   );
 }
 
