c4e2612e3f38a84b5080782978b53667215265ae
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-11-18 13:42:35 +0300

    add insight options for flourish block

--- wp-content/plugins/hurumap/src/FlourishBlock/Save.js
+++ wp-content/plugins/hurumap/src/FlourishBlock/Save.js
@@ -8,6 +8,13 @@ function Save({ attributes }) {
       data-chart-id={attributes.chartId}
       data-chart-title={attributes.title}
       data-chart-description={attributes.description}
+      data-show-insight={attributes.showInsight}
+      data-insight-title={attributes.insightTitle}
+      data-insight-summary={attributes.insightSummary}
+      data-data-link-title={attributes.dataLinkTitle}
+      data-analysis-country={attributes.analysisCountry}
+      data-analysis-link-title={attributes.analysisLinkTitle}
+      data-data-geoId={attributes.dataGeoId}
     />
   );
 }
@@ -16,7 +23,14 @@ Save.propTypes = {
   attributes: propTypes.shape({
     chartId: propTypes.chartId,
     title: propTypes.string,
-    description: propTypes.string
+    description: propTypes.string,
+    showInsight: propTypes.bool,
+    insightSummary: propTypes.string,
+    insightTitle: propTypes.string,
+    analysisCountry: propTypes.string,
+    analysisLinkTitle: propTypes.string,
+    dataLinkTitle: propTypes.string,
+    dataGeoId: propTypes.string
   }).isRequired
 };
 
