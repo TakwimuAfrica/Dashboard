34c88bc27da7a01cceae885aff8cdf6ac202d8f9
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-11-21 14:11:44 +0300

    merge version

--- wp-content/plugins/hurumap/db.php
+++ wp-content/plugins/hurumap/db.php
@@ -100,34 +100,6 @@ function migrate() {
     }
     if ($version < 4) {
         $wpdb->query("DROP TABLE IF EXISTS `{$wpdb->base_prefix}hurumap_charts`");
-
-        $sql = "CREATE TABLE `{$wpdb->base_prefix}hurumap_charts`(
-            `id`         varchar(45) NOT NULL ,
-            `section`    varchar(45) NULL ,
-            `title`      varchar(255) NOT NULL ,
-            `subtitle`   varchar(255) NOT NULL ,
-            `visual`     json NOT NULL ,
-            `stat`       json NULL ,
-            `description`       json NULL ,
-            `source`       json NULL ,
-            `published` tinyint NOT NULL DEFAULT 0,
-            `created_at` datetime NOT NULL DEFAULT NOW() ON UPDATE NOW(),
-            `updated_at` datetime NOT NULL DEFAULT NOW(),
-
-            PRIMARY KEY (`id`),
-            KEY `fkIdx_19` (`section`),
-            CONSTRAINT `FK_19` FOREIGN KEY `fkIdx_19` (`section`) REFERENCES `{$wpdb->base_prefix}chart_sections` (`id`)
-            ) $charset_collate;";
-        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-        dbDelta($sql);
-        $success = empty($wpdb->last_error);
-
-        update_site_option('hurumap_data_db_version', 4);
-
-    }
-
-    if ($version < 4) {
-        $wpdb->query("DROP TABLE IF EXISTS `{$wpdb->base_prefix}hurumap_charts`");
         $wpdb->query("DROP TABLE IF EXISTS `{$wpdb->base_prefix}chart_sections`");
 
         $sql = "CREATE TABLE `{$wpdb->base_prefix}chart_sections`(
