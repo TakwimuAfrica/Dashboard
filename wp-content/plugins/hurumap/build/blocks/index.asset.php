89f60e44e975ea75e1d995b9855f3b7fb650e98e
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-11-27 14:04:04 +0300

    remove build from git

--- wp-content/plugins/hurumap/build/blocks/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '47252df04d44e431f1f088d720d3c78c');
