2323cf3ba4c0d3842556e81394ce3ff8bf7e913a
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-12-05 08:38:43 +0300

    Initial

--- wp-content/plugins/hurumap/index.php
+++ wp-content/plugins/hurumap/index.php
@@ -15,6 +15,7 @@ defined( 'ABSPATH' ) || exit;
 require plugin_dir_path( __FILE__ ) . 'db.php';
 require plugin_dir_path( __FILE__ ) . 'api.php';
 require plugin_dir_path( __FILE__ ) . 'acf.php';
+require plugin_dir_path( __FILE__ ) . 'data.php';
 require plugin_dir_path( __FILE__ ) . 'posttypes.php';
  
 
