dc21974c162e8b3da0e1ad2d25a58e87e2eb27b0
Khadija Mahanga <khadijamahanga@y7mail.com>
2020-02-07 17:17:09 +0300

    Update functions.php

--- wp-content/themes/hurumap/functions.php
+++ wp-content/themes/hurumap/functions.php
@@ -240,16 +240,8 @@ function hurumap_register_required_plugins() {
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true,
 			'force_deactivation' => true,
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 		),
-
-        /* Once we deploy the plugin to WordPress Plugin Repository, then we can use this format
-            array(
-                'name'      => 'HURUmap',
-                'slug'      => 'hurumap',
-                'required'  => false,
-            ),
-        */
+        // Once we deploy the plugin to WordPress Plugin Repository, then we won't specify plugin source
 
 	);
 
