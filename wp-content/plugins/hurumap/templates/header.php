d7fa7c4a3cc79f7c2007c543863fae1fcf2fc323
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-02-07 15:35:23 +0300

    remove templates folder

--- wp-content/plugins/hurumap/templates/header.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html <?php language_attributes(); ?> style="margin: 0 !important;">
-
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
--- wp-content/plugins/hurumap/templates/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-get_header();
-?>
-
-<div id="root"></div>
