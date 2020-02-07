35270eb038a435372f673138d8eb5b0f5e84bc21
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-02-07 16:19:20 +0300

    register required plugins

--- wp-content/themes/hurumap/functions.php
+++ wp-content/themes/hurumap/functions.php
@@ -212,3 +212,57 @@ function exclude_page_posts($post_types) {
 
 add_filter( 'ep_indexable_post_types', 'exclude_page_posts');
 add_filter ('ep_searchable_post_types', 'exclude_page_posts');
+
+/**
+ * Include the TGM_Plugin_Activation class.
+ */
+require_once get_template_directory() . 'class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'hurumap_register_required_plugins' );
+
+/**
+ * Register the required plugins for this theme.
+ */
+
+function hurumap_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+		//include a plugin bundled with a theme.
+		array(
+			'name'               => 'HURUmap', 
+			'slug'               => 'hurumap',
+			'source'             => get_template_directory() . '/lib/plugins/hurumap.zip', 
+			'required'           => true,
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => true,
+			'force_deactivation' => true,
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+		),
+
+        /* Once we deploy the plugin to WordPress Plugin Repository, then we can use this format
+            array(
+                'name'      => 'HURUmap',
+                'slug'      => 'hurumap',
+                'required'  => false,
+            ),
+        */
+
+	);
+
+	$config = array(
+		'id'           => 'hurumap',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
